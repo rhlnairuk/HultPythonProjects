e2b809a11c7d31f1e70dfa719aed6865771b9575
Rahul Nair <rhlnair87@gmail.com>
2024-06-05 15:38:04 +0100

    In class tasks

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from list_of_countries import COUNTRIES
 countries = list(COUNTRIES.keys())
 country_to_guess = random.choice(countries)
 
-print(country_to_guess)
+guess_the_capital = input(f"Guess the capital of {country_to_guess}: ")
+
+if guess_the_capital.lower() == COUNTRIES[country_to_guess].lower():
+    print(f"You are Correct! The capital is {COUNTRIES[country_to_guess]}")
+else:
+    print(f"Incorrect! the capital is {COUNTRIES[country_to_guess]}")
