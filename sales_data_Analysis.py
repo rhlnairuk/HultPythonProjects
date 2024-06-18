1531e3bc56bb5cc6218d7739fd125e4cad5053b5
Rahul Nair <rhlnair87@gmail.com>
2024-06-18 23:37:31 +0100

    fixed missing plots

--- sales_data_Analysis.py
+++ sales_data_Analysis.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 def main():
     with open("salesdata.csv", "r") as my_file:
         sales_data = list(csv.reader(my_file,delimiter=","))
+    print(sales_data)
     month_number = [int(sales_data[i][0]) for i in range(1,len(sales_data))]
     # Units Sold
     face_cream = [int(sales_data[i][1]) for i in range(1,len(sales_data))]
     face_wash = [int(sales_data[i][2]) for i in range(1,len(sales_data))]
     tooth_paste = [int(sales_data[i][3]) for i in range(1,len(sales_data))]
-    moisturizer = [int(sales_data[i][4]) for i in range(1,len(sales_data))]
+    bathing_soap = [int(sales_data[i][4]) for i in range(1,len(sales_data))]
+    shampoo = [int(sales_data[i][5]) for i in range(1,len(sales_data))]
+    moisturizer = [int(sales_data[i][6]) for i in range(1,len(sales_data))]
 
     # Profit
     total_profit = [int(sales_data[i][8]) for i in range(1,len(sales_data))]
@@ -25,6 +28,8 @@ def main():
     plt.plot(month_number, face_cream, label='Face cream Sales Data', marker='o')
     plt.plot(month_number, face_wash, label='Face wash Sales Data', marker='o')
     plt.plot(month_number, tooth_paste, label='Tooth paste Sales Data', marker='o')
+    plt.plot(month_number, bathing_soap, label='Bathing Soap Sales Data', marker='o')
+    plt.plot(month_number, shampoo, label='Shampoo Sales Data', marker='o')
     plt.plot(month_number, moisturizer, label='Moisturizer Sales Data', marker='o')
     plt.xlabel('Month Number')
     plt.ylabel('Sales Units in number')
